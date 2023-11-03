339b82fa13dfb2d2c97ed3a0f743068d6387f19c
Mollomm1 <mollomm1@example.com>
2023-11-03 11:48:11 +0100

    add gpu support (foss drivers only)

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ cp -r GamingOnCodespaces/root/config/* Save
 
 json_file="GamingOnCodespaces/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=GamingOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped gamingoncodespaces
+    docker run -d --name=GamingOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --device /dev/dri:/dev/dri --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped gamingoncodespaces
 else
-    docker run -d --name=GamingOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped gamingoncodespaces
+    docker run -d --name=GamingOnCodespaces -e PUID=1000 -e PGID=1000 --device /dev/dri:/dev/dri --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped gamingoncodespaces
 fi
 clear
 echo "INSTALL FINISHED! Check Port Tab"
